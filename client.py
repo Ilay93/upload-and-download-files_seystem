8f4d8bea2e5f1ab81327490520612e17a9d6822e
Ilay Spector <ilaisp4@gmail.com>
2025-06-27 16:13:57 +0300

    add download option

--- client.py
+++ client.py
@@ -2,9 +2,8 @@ import socket
 import os
 
 
-
 def send_file(sock: socket.socket, file_path: str, file_name: str) -> None:
-    
+    sock.send("upload".encode())
     sock.send(file_name.encode())
 
     file_size = os.path.getsize(file_path)
@@ -12,17 +11,60 @@ def send_file(sock: socket.socket, file_path: str, file_name: str) -> None:
 
     with open(os.path.join(file_path, file_name), mode="rb") as file:
         data = file.read()
-    
+
     sock.sendall(data)
 
-if __name__ == "__main__":
+
+def download_file_from_server(sock: socket.socket, download_file_name: str, save_file_path: str) -> None:
+    sock.send("download".encode())
+    sock.send(download_file_name.encode())
+    
+    status = sock.recv(1024).decode()
+    if status == "1":
+        size = int(sock.recv(1024).decode())
+        data_remaining = size
+        with open(os.path.join(save_file_path, download_file_name), "wb") as file:
+            while data_remaining > 0:
+                chunk = sock.recv(1024)
+                file.write(chunk)
+                data_remaining -= len(chunk)
+    else:
+        print("The file you wrote does not exist")
+    
+
+
+def main():
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     my_socket.connect(("127.0.0.1", 8080))
-    file_path = input("enter file path (without the name of the file): ")
-    file_name = input("Enter file name: ")
-    if not os.path.exists(os.path.join(file_path, file_name)):
-        my_socket.close()
-        raise Exception(f"The path you specified {os.path.join(file_path, file_name)} does not exist")
-    send_file(my_socket, file_path, file_name)
+ 
+    upload_or_download_option = input("Enter action download/upload: ")
+    while upload_or_download_option != "download" and upload_or_download_option != "upload":
+        upload_or_download_option = input("You didn't enter one of the options. Enter action download/upload: ")
+
+    if upload_or_download_option == "upload":
+        file_path = input("enter the path for the file you want to upload (without the name of the file): ")
+        file_name = input("Enter the name of the file you want to download: ")
+
+        while not os.path.exists(os.path.join(file_path, file_name)):
+            print(f"The file location: {os.path.join(file_path, file_name)} you entered does no exist ")
+            file_path = input("enter file path (without the name of the file): ")
+            file_name = input("Enter file name: ")
+
+        send_file(my_socket, file_path, file_name)
+    
+    else:
+        file_to_download = input("Enter the name of the file you want to download: ").strip()
+        while file_to_download == "":
+            file_to_download = input("You didn't write a file name, Enter the name of the file you want to download: ")
+        folder_path_to_save = input("Enter the location of the folder you want to save the file in: ")
+        
+        if os.path.isdir(folder_path_to_save):
+            download_file_from_server(my_socket, file_to_download, folder_path_to_save)
+        else:
+            print("You didn't specify a proper folder location")
     
     my_socket.close()
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,23 +1,57 @@
 import socket
 import os
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("127.0.0.1", 800))
 
-server_socket.listen(5)
+def get_file_data_from_client(client_sock: socket.socket):
+    file_name = client_sock.recv(1024).decode()
+    file_size = int(client_sock.recv(1024).decode())
+    
+    write_file_from_client(client_sock, os.path.join("files_stored", file_name), file_size)
 
-while True:
-    client_socket, addr = server_socket.accept()
 
-
-    file_name = client_socket.recv(1024).decode()
-    file_size = int(client_socket.recv(1024).decode())
-    data_left = file_size
-
-    with open(os.path.join("files_stored", file_name), "wb") as file:
-        while data_left > 0:
-            chunk = client_socket.recv(1024)
+def write_file_from_client(client_sock: socket.socket, path: str, size: int) -> None:
+    with open(path, "wb") as file:
+        while size > 0:
+            chunk = client_sock.recv(1024)
             file.write(chunk)
-            data_left -= 1024
-        
-    client_socket.close()
+            size -= len(chunk)
+
+
+def send_file_data(client_sock: socket.socket) -> None:
+    file_name = client_sock.recv(1024).decode()
+    file_full_path = os.path.join("files_stored", file_name)
+    if os.path.isfile(file_full_path):
+        client_sock.send("1".encode())
+        file_size = os.path.getsize(file_full_path)
+        data = get_file_data(file_full_path)
+        client_sock.send(str(file_size).encode())
+        client_sock.sendall(data)
+    else:
+        client_sock.send("0".encode())
+
+
+def get_file_data(file_path: str) -> bytes:
+    with open(file_path, "rb") as file:
+        data = file.read()
+    return data
+
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("127.0.0.1", 8080))
+
+    server_socket.listen(5)
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        method = client_socket.recv(1024).decode()
+
+        if method == "upload":
+            get_file_data_from_client(client_socket)
+        elif method == "download":
+            send_file_data(client_socket)
+        else:
+            print(method)
+
+if __name__ == "__main__":
+    main()
